05a0d2393d0589a7e7e3e4c691e6dac3b2022ba1
andresbdz <andresbalcazar.bdz@gmail.com>
2025-11-17 20:31:56 -0500

    Bind Flask to Railway PORT

--- app.py
+++ app.py
@@ -218,5 +218,8 @@ def download(filename):
     else:
         return "File not found", 404
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
